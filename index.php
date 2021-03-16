3a45dd716131e3372a440aa87a639729a0cefba7
Lorran Oliveira <58430075+lorran-xo@users.noreply.github.com>
2021-03-16 10:04:43 -0300

    rotas html para fluir as páginas
    ./index.php -> Página inicial que lista os animais do canil;
    ./cadastro.php -> Formulário que será possível cadastrar animais novos no canil;
    ./saidas.php -> Terá a listagem dos animais disponíveis e será possível retirar animais da tabela para adoção.

--- index.php
+++ index.php
@@ -21,15 +21,34 @@
 		<div class="cadastro">
 			<center><h2> Gerenciador de Canil </h2></center>
 			<form class="form" method="POST" action="./functions.php">
-				<center><h3> Cadastro de Animais</h3></center>
-				<input class="field" name="tipo" placeholder="Tipo">
-				<input class="field" name="raca" placeholder="Raça">
-				<input class="field" name="porte" placeholder="Porte">
-				<input class="field" name="sexo" placeholder="Sexo">
-				<input class="field" name="idade" placeholder="Idade">
-				<input class="field" name="acao" type="submit" value="Cadastrar">
+				<center><h3> Listagem - Animais disponíveis</h3></center>
+					<table border="1">
+					    <tr>
+					        <td>Animal</td>
+					        <td>Raça</td>
+					        <td>Porte</td>
+					        <td>Sexo</td>
+					        <td>Idade</td>
+					    </tr>
+					    <tr>
+					        <td>Cachorro</td>
+					        <td>Pinscher</td>
+					        <td>Pequeno</td>
+					        <td>Masculino</td>
+					        <td>1</td>
+					    </tr>
+					    <tr>
+					        <td>Gato</td>
+					        <td>Sphynx</td>
+					        <td>Médio</td>
+					        <td>Fêmea</td>
+					        <td>3</td>
+					    </tr>
+					</table>
+				<input class="field" name="acao" type="submit" value="Editar Dados">
 			</form>
-			 <a href="./page2.php"><input name="acao" type="submit" value="Retirar Animal"></a> 
+			 <a href="./cadastro.php"><input name="acao" type="submit" value="Cadastrar"></a>
+			 <a href="./saidas.php"><input name="acao" type="submit" value="Retirar"></a> 
 		</div>
 
 	</section>
